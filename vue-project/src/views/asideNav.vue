46b23aaa7c89000456833186f94cc04b3bbffa21
黎锦年 <3514328912@qq.com>
2025-03-28 19:07:53 +0800

    完善了服务器页面逻辑

--- vue-project/src/views/asideNav.vue
+++ vue-project/src/views/asideNav.vue
@@ -1,56 +1,77 @@
 <template>
-    <div class="aside" style="height: 100%; width: 100%;">
-        
-        <el-row class="tac" style="height: 100%; width: 100%;">
-            <el-col :span="12" style="height: 100%; width: 100%;">
-                <el-menu default-active="this.$route.path" router class="el-menu-vertical-demo" @open="handleOpen"
-                    @close="handleClose" background-color="#111" text-color="#fff" active-text-color="#ffd04b"
-                    style="height: 100%; width: 100%;">
-                    <el-submenu index="/PageOne">
-                        <template slot="title">
-                            <i class="el-icon-location"></i>
-                            <span>导航一</span>
-                        </template>
-                        <el-menu-item-group>
-                             <!-- 添加服务器总数显示区域 -->
-                             <div class="server - total" style="font-size: 26px; padding: 10px 0 10px 20px; color: white; text-align: left;">
-                                <span style="font-weight: bold;">服务器总数：</span><span style="font-weight: bold; color: #999;">2</span>
-                            </div>
-                            <el-menu-item :index="`/monitor/${$route.params.host_name}/pageone`"
-                            style="
-                                    margin-top: 20px;
-                                    border-radius: 5px;
-                                    padding: 20px;
-                                    background-color: #222;
-                                "
-                            >服务器1</el-menu-item>
-                            <el-menu-item :index="`/monitor/${$route.params.host_name}/pagetwo`"
-                            style="
-                                    margin-top: 10px;
-                                    border-radius: 5px;
-                                    padding: 20px;
-                                    background-color: #222;
-                                "
-                            >服务器2</el-menu-item>
-                        </el-menu-item-group>
-                    </el-submenu>
-
-                </el-menu>
-            </el-col>
-        </el-row>
-    </div>
+<el-row class="tac">
+  <el-col :span="12">
+      <h3 class="mb-2" style="color: white">服务器列表</h3>
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="el-menu-vertical-demo"
+        default-active="2"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+      >      
+        <el-menu-item v-for="(server,index) in serverList" :key=server.id index="server.id">
+          <el-icon><setting /></el-icon>
+          <span>服务器{{ server.id }}</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+</el-row>
 </template>
 
 <script>
-
 export default {
-    methods: {
-        handleOpen(key, keyPath) {
-            console.log(key, keyPath);
-        },
-        handleClose(key, keyPath) {
-            console.log(key, keyPath);
+    data() {
+        return {
+            serverList: [],
+            token: localStorage.getItem('token') || '',  // 从 localStorage 获取 token
         }
+    },
+    mounted() {
+        this.getserverList();
+    },
+    methods:{
+        async getserverList() {
+           try {
+                const response = await fetch("http://localhost:8080/agent/list", {
+                    method: 'GET',  
+                    
+                    headers: {
+                        
+                        Authorization: this.token,  // 携带 token
+                    },
+                }).then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response;
+                })
+                
+                const data = await response.json();
+                this.serverList = data.map(item => ({
+                id: item.id,
+                host_name: item.host_name,
+                os: item.os,
+                platform: item.platform,
+                kernel_arch:item.kernel_arch,
+
+                }))
+                console.log(data);
+                console.log(this.serverList);
+            } catch (error) {
+                console.error('获取服务器数据失败:', error.response?.data || error.message);
+            }
+        },
+        // selectServer(server) {
+        //     this.selectedServer = server;
+        // }
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
     }
 }
 </script>
