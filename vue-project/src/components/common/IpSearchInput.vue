1317ead6d479666267d46cb66b037ce96b8be69f
czh01 <2680728792@qq.com>
2025-06-16 13:14:30 +0800

    统一所有请求的ip

--- vue-project/src/components/common/IpSearchInput.vue
+++ vue-project/src/components/common/IpSearchInput.vue
@@ -64,7 +64,7 @@ const loadIpHistory = async () => {
       return getDefaultIpList();
     }
     
-    const response = await fetch('http://47.86.232.20:8080/agent/list', {
+    const response = await fetch('http://113.44.170.52:8080/agent/list', {
       method: 'GET',
       headers: {
         'Authorization': token,
